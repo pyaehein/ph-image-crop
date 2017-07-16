31f3402308c0b4930af173126abf907b3bcfebce
Pyae Hein <pyaehein@Pyae-Heins-MacBook-Pro.local>
2017-07-16 14:44:45 +0630

    Added example and changed with parameter

--- Readme.md
+++ Readme.md
@@ -4,10 +4,8 @@ This plug-in need jQuery, Bootstrap and Cropper
 
 ### Usage
 
-Include css and script file.
+Include script file.
 ```html
-<link rel="stylesheet" href="/ph-image-crop/css/ph-image-crop.min.css">
-
 <script src="/ph-image-crop/js/ph-image-crop.min.js"></script>
 ```
 
@@ -15,12 +13,14 @@ Use modal box name as "ph-image-crop-modal"
 and use image id as "crop-image"
 
 ```html
+<div class="image-crop"><div>
+
 <div class="modal fade" id="ph-image-crop-modal" role="dialog">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-body">
                 <div>
-                    <img id="crop-image">
+                    <img style="max-width: 100%" id="crop-image">
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Okay</button>
@@ -31,35 +31,37 @@ and use image id as "crop-image"
 </div>
 ```
 ##
-Use this code for preview image. If you want to set old image, set it in old-thumbnail's src.  
+If you want to use canvas, don't forget to set fileName.
 
-For image cropping and some settings  
+Initialize with phImageCrop method.
 
-ratio = data-ratio,  
-maximum image width = data-width  
-maximum image height = data-height  
-using canvas or base64 = data-canvas (true - canvas, false - base64)
+If you want to use with base64
+```js
+$('.image-crop').phImageCrop({
+    defaultImage: '',
+    type: 'base64',
+    name: 'test_images[]',
+    cropper: {
+        aspectRatio: 300/100,
+        minCropBoxWidth: 300,
+        minCropBoxHeight: 100
+    }
+})
+```
 
-If you want to use canvas, don't forget to put name field in your file input. Or if you use base64, u don't need to do it.
-
-```html
-<div class="ph-image-crop" data-ratio="300/100" data-canvas="true" data-width="300" data-height="100" >
-    <div class="ph-img-thumbnail" style="padding-bottom: 10px">
-        <img class="old-thumbnail" src="">
-        <img class="ph-thumbnail">
-        <img class="base-thumbnail">
-        <input class="ph-image"  type="hidden" name="ph_images[]">
-    </div>
-    <div>
-        <label class="btn btn-default">
-            <span class="select-image">Select an image</span>
-            <span class="change-image">Change</span>
-            <input class="image-file" name="ph_image_file" type="file" accept="image/*">
-        </label>
-        <button class="btn btn-default crop-button">Crop</button>
-        <button class="btn btn-default remove-button">Remove</button>
-    </div>
-</div>
+If you want to use it with canvas
+```js
+$('.image-crop').phImageCrop({
+    defaultImage: '',
+    type: 'canvas',
+    name: 'my_abc_image',
+    fileName: 'my_abc', //File name is only need in type = canvas
+    cropper: {
+        aspectRatio: 300/300,
+        minCropBoxWidth: 300,
+        minCropBoxHeight: 300
+    }
+})
 ```
 
 
--- js/ph-image-crop.js
+++ js/ph-image-crop.js
@@ -1,80 +1,125 @@
-var cropButton = $(".crop-button")
-var cropImage = $("#crop-image")
-var removeButton = $(".remove-button")
-var imageFile = $(".image-file")
-var cropModal = $("#ph-image-crop-modal")
-var imgThumbnail = $(".ph-thumbnail")
-var currentImage, currentInput, currentSelectInput, currentRatio = 1, currentWidth = 1, currentHeight = 1, currentCanvas = 'false';
+(function ($) {
+    $.fn.phImageCrop = function (options) {
 
-$(document).ready(function() {
-    removeButton.on("click", function() {
-        $(this).siblings("label").children(".image-file").val("")
-        $(this).parent().siblings(".ph-img-thumbnail").children(".ph-thumbnail").attr("src", "")
-        $(this).hide()
-        $(this).parent().siblings(".ph-img-thumbnail").children(".old-thumbnail").show()
-        $(this).siblings("label").children(".select-image").show()
-        $(this).siblings("label").children(".change-image").hide()
-        $(this).siblings(".crop-button").hide()
-    })
+        var settings = $.extend({
+            defaultImage: '',
+            name: 'ph_images[]',
+            fileName: '',
+            type: 'base64',
+            cropper: {
+                aspectRatio: 1,
+                minCropBoxWidth: 1,
+                minCropBoxHeight: 1
+            }
+        }, options)
 
-    imgThumbnail.on("load", function() {
-        $(this).siblings(".old-thumbnail").hide()
-    })
+        return this.each(function () {
 
-    cropButton.on("click", function() {
-        cropImage.attr("src", $(this).parent().siblings(".ph-img-thumbnail").children(".base-thumbnail").attr("src")),
-        currentImage = $(this).parent().siblings(".ph-img-thumbnail").children(".ph-thumbnail")
-        currentInput = $(this).parent().siblings(".ph-img-thumbnail").children(".ph-image")
-        currentRatio = $(this).parent().parent().attr('data-ratio')
-        currentWidth = $(this).parent().parent().attr('data-width')
-        currentHeight = $(this).parent().parent().attr('data-height')
-        currentCanvas = $(this).parent().parent().attr('data-canvas')
-        cropModal.modal("show")
-    })
+            // Increase index for multiple instance. We can't use each's index because multiple calling can be happen.
+            if(typeof window.phImageIndex !== 'undefined') {
+                window.phImageIndex = window.phImageIndex + 1
+            } else {
+                window.phImageIndex = 0
+            }
+            var index = window.phImageIndex
 
-    imageFile.on("change", function() {
-        var that = $(this);
-        that.siblings(".change-image").show()
-        that.siblings(".select-image").hide()
-        that.parent().siblings(".crop-button").show()
-        that.parent().siblings(".remove-button").show();
-        var file = that[0].files[0]
-        var reader = new FileReader;
-        reader.addEventListener("load", function() {
-            cropImage.attr("src", reader.result)
-            that.parent().parent().siblings(".ph-img-thumbnail").children(".base-thumbnail").attr("src", reader.result)
-        }, false)
+            var phHtml = '<div class="ph-img-thumbnail" style="padding-bottom: 10px">' +
+                '<img style="padding: 4px; border: 1px solid #ddd; border-radius: 4px;" class="old-thumbnail" id="ph-old-' + index + '" src="' + settings.defaultImage + '">' +
+                '<img style="padding: 4px; border: 1px solid #ddd; border-radius: 4px; display: none" class="ph-thumbnail" id="ph-thumb-' + index + '">' +
+                '<img style="display: none" class="base-thumbnail" id="ph-base-' + index + '">' +
+                '<input class="ph-image" id="ph-image-' + index + '" type="hidden" name="' + settings.name + '">' +
+                '</div>' +
+                '<div>' +
+                '<label class="btn btn-default">' +
+                '<span class="select-image" id="select-image-' + index + '">Select an image</span>' +
+                '<span style="display: none" class="change-image" id="change-image-' + index + '">Change</span>' +
+                '<input style="display: none" class="image-file" id="ph-image-file-' + index + '" ' + ((settings.fileName !== '') ? 'name="' + settings.fileName + '" ' : '') + 'type="file" accept="image/*">' +
+                '</label>' +
+                '<button style="display: none" id="crop-button-' + index + '" class="btn btn-default crop-button">Crop</button>' +
+                '<button style="display: none" id="remove-button-' + index + '" class="btn btn-default remove-button">Remove</button>' +
+                '</div>'
 
-        if (file) {
-            reader.readAsDataURL(file);
-        }
+            $(this).addClass('ph-image-crop')
+                .attr('id', 'ph-image-crop-' + index)
+                .attr('data-ratio', settings.cropper.aspectRatio)
+                .attr('data-width', settings.cropper.minCropBoxWidth)
+                .attr('data-height', settings.cropper.minCropBoxHeight)
+                .attr('data-type', settings.type);
+            $(this).html(phHtml)
 
-        currentImage = $(this).parent().parent().siblings(".ph-img-thumbnail").children(".ph-thumbnail")
-        currentInput = $(this).parent().parent().siblings(".ph-img-thumbnail").children(".ph-image")
-        currentRatio = $(this).parent().parent().parent().attr('data-ratio')
-        currentWidth = $(this).parent().parent().parent().attr('data-width')
-        currentHeight = $(this).parent().parent().parent().attr('data-height')
-        currentCanvas = $(this).parent().parent().parent().attr('data-canvas')
-        currentSelectInput = $(this)
-        cropModal.modal("show")
-    })
+            //Remove button click for each item
+            $("#remove-button-" + index).on("click", function() {
+                $("#ph-image-file-" + index).val("")
+                $("#ph-thumb-" + index).attr("src", "")
+                $("#ph-thumb-" + index).hide()
+                $(this).hide()
+                $(this).parent().removeClass("btn-group")
+                $("#ph-old-" + index).show()
+                $("#select-image-" + index).show()
+                $("#change-image-" + index).hide()
+                $("#crop-button-" + index).hide()
+            })
 
-    cropModal.on("shown.bs.modal", function() {
-        cropImage.cropper({
-            aspectRatio: eval(currentRatio),
-            minCropBoxWidth: eval(currentWidth),
-            minCropBoxHeight: eval(currentHeight)
+            //Hide default image when we browse image
+            $("#ph-thumb-" + index).on("load", function() {
+                $("#ph-old-" + index).hide()
+                $(this).show()
+            })
+
+            //Set Crop Index for placing cropped image
+            $("#crop-button-" + index).on("click", function() {
+                $("#crop-image").attr("src", $("#ph-base-" + index).attr("src"))
+                window.phImageCropIndex = index
+                $("#ph-image-crop-modal").modal("show")
+            })
+
+            //When we detect image browse read base64, put it to cropper modal
+            $("#ph-image-file-" + index).on("change", function() {
+                var that = $(this);
+                that.parent().parent().addClass("btn-group")
+                $("#change-image-" + index).show()
+                $("#select-image-" + index).hide()
+                $("#crop-button-" + index).show()
+                $("#remove-button-" + index).show();
+                var file = that[0].files[0]
+                var reader = new FileReader;
+                reader.addEventListener("load", function() {
+                    $("#crop-image").attr("src", reader.result)
+                    $("#ph-base-" + index).attr("src", reader.result)
+                }, false)
+
+                if (file) {
+                    reader.readAsDataURL(file);
+                }
+                window.phImageCropIndex = index
+                $("#ph-image-crop-modal").modal("show")
+            })
+
+            //We only need to run once it, because we use same modal for all images.
+            if(window.phImageIndex === 0) {
+                $("#ph-image-crop-modal").on("shown.bs.modal", function() {
+                    var tempCrop = $("#ph-image-crop-" + window.phImageCropIndex)
+                    $("#crop-image").cropper({
+                        aspectRatio: tempCrop.attr('data-ratio'),
+                        minCropBoxWidth: tempCrop.attr('data-width'),
+                        minCropBoxHeight: tempCrop.attr('data-height')
+                    })
+                }).on("hidden.bs.modal", function() {
+                    var dataURL = $("#crop-image").cropper("getCroppedCanvas", {
+                        fillColor: '#fff'
+                    }).toDataURL("image/jpeg");
+                    $("#ph-thumb-" + window.phImageCropIndex).attr("src", dataURL)
+                    if(settings.type === 'canvas') {
+                        $("#ph-image-" + window.phImageCropIndex).val($("#crop-image").cropper("getCropBoxData "))
+                    } else if(settings.type === 'base64') {
+                        $("#ph-image-" + window.phImageCropIndex).val(dataURL)
+                        $("#ph-image-file-" + window.phImageCropIndex).val('')
+                    }
+                    $("#crop-image").cropper("destroy")
+                })
+            }
+
         })
-    }).on("hidden.bs.modal", function() {
-        var dataURL = cropImage.cropper("getCroppedCanvas").toDataURL("image/jpeg");
-        currentImage.attr("src", dataURL)
-        if(currentCanvas === 'true') {
-            currentInput.val(cropImage.cropper("getCropBoxData "))
-        } else {
-            currentInput.val(dataURL)
-            currentSelectInput.val('')
-        }
-        cropImage.cropper("destroy")
-    })
 
-});
+    }
+}(jQuery))
